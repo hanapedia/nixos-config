c8f9f3dce8904577d4c9665256a9c14501b1d698
hanapedia <hiroki11hanada@gmail.com>
2025-05-03 19:11:30 +0900

    Update deprecated field key

--- modules/user.nix
+++ modules/user.nix
@@ -8,8 +8,8 @@
     shell = pkgs.bash;
   };
 
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "hanapedia";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "hanapedia";
 
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
