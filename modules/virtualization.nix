a2bdf372c6e4adddb56a145bc59c868b74dc38ec
Hiroki Hanada <hiroki-hanada@cybozu.co.jp>
2025-08-10 16:37:12 +0900

    configure registry mirror

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,7 +1,20 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   virtualisation.podman.enable = true;
 
   environment.systemPackages = with pkgs; [
     podman
   ];
+
+  virtualisation.containers.registries.insecure = [ "nixos-server-r596:5000" ];
+
+  environment.etc."containers/registries.conf".text = lib.mkForce ''
+    unqualified-search-registries = ["docker.io"]
+
+    [[registry]]
+    prefix = "docker.io"
+    location = "docker.io"
+      [[registry.mirror]]
+      location = "nixos-server-r596:5000"
+      insecure = true
+  '';
 }
