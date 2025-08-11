5925a1d1105127f67fb1418e01cd803b5c6f3d23
Hiroki Hanada <hiroki-hanada@cybozu.co.jp>
2025-08-11 13:22:42 +0900

    remove fish workaround

--- modules/cli.nix
+++ modules/cli.nix
@@ -4,14 +4,8 @@
 
   programs.git.enable = true;
   programs.fish.enable = true;
-
-  programs.bash.interactiveShellInit = ''
-    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-    then
-      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-    fi
-  '';
+  users.defaultUserShell = pkgs.fish;
+  environment.shells = [ pkgs.fish pkgs.bashInteractive ];
 
   programs.tmux.enable = true;
 
