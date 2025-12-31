9ad2712f4c2b35cdac6fccb37ec901cc1d6603ca
Hiroki Hanada <hiroki-hanada@cybozu.co.jp>
2025-12-31 11:07:59 +0900

    install some tools by default
    Signed-off-by: Hiroki Hanada <hiroki-hanada@cybozu.co.jp>

--- modules/languages.nix
+++ modules/languages.nix
@@ -1,5 +1,20 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     go
+    zig
+    zls
+    llvm
+    lldb
+    clang
+    bear
+    bpftools
+    bpftrace
+    clang-tools
+    gnumake
+    libbpf
+    /* llvmPackages_18.clang-unwrapped */
+    /* llvmPackages_18.bintools */
+    pkg-config
+
   ];
 }
