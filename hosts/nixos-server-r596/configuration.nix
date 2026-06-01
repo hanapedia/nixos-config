115a0e4f20dade997a07ad14bc72634f1bac59f3
Hiroki Hanada <hiroki-hanada@cybozu.co.jp>
2026-06-02 01:32:48 +0900

    allow private address
    Signed-off-by: Hiroki Hanada <hiroki-hanada@cybozu.co.jp>

--- hosts/nixos-server-r596/configuration.nix
+++ hosts/nixos-server-r596/configuration.nix
@@ -27,4 +27,22 @@
     enable = true;
     enable32Bit = true;
   };
+
+  # allow privates address
+  networking.firewall.extraCommands = ''
+    iptables -I FORWARD -s 10.0.0.0/8 -j ACCEPT
+    iptables -I FORWARD -d 10.0.0.0/8 -j ACCEPT
+    iptables -I FORWARD -s 172.16.0.0/12 -j ACCEPT
+    iptables -I FORWARD -d 172.16.0.0/12 -j ACCEPT
+    iptables -I FORWARD -s 192.168.0.0/16 -j ACCEPT
+    iptables -I FORWARD -d 192.168.0.0/16 -j ACCEPT
+  '';
+  networking.firewall.extraStopCommands = ''
+    iptables -D FORWARD -s 10.0.0.0/8 -j ACCEPT || true
+    iptables -D FORWARD -d 10.0.0.0/8 -j ACCEPT || true
+    iptables -D FORWARD -s 172.16.0.0/12 -j ACCEPT || true
+    iptables -D FORWARD -d 172.16.0.0/12 -j ACCEPT || true
+    iptables -D FORWARD -s 192.168.0.0/16 -j ACCEPT || true
+    iptables -D FORWARD -d 192.168.0.0/16 -j ACCEPT || true
+  '';
 }
